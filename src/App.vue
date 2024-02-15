5260920f43a66ab6ff3861cb42c3f4a665566091
Sainitin D <s.daverpally@gmail.com>
2024-02-14 21:28:15 -0500

    add initial header and add capabaility to drag and place walls on the board

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
   <div id="app">
+    <MainHeader />
     <div id="board" v-if="this.board.length !== 0">
       <div class="row" v-for="(row, rowIdx) in board" :key="rowIdx">
         <CellItem
           v-for="cellInfo in row"
           :key="cellInfo.id"
           :cellInfo="cellInfo"
-          @click="dfs(cellInfo)"
+          @click="this.modifyCellTypeClick(cellInfo)"
+          @mousedown="
+            this.isMouseDown = true;
+            console.log('Mouse Down');
+          "
+          @mouseup="
+            this.isMouseDown = false;
+            this.modifyCellTypeClick(cellInfo);
+            console.log('Mouse Up');
+          "
+          @mouseleave="this.modifyCellType(cellInfo)"
         />
       </div>
+      <!-- @mouseleave="
+            this.isMouseDown
+              ? (cellInfo.cellType = CellType.Wall)
+              : console.log('whoa!')
+          " -->
+      <!-- @click="dfs(this.board[12][5])" -->
     </div>
   </div>
 </template>
@@ -16,8 +33,8 @@
 <script>
 import { ref } from "vue";
 import CellItem from "./components/CellItem.vue";
-import { CellType } from "./helper/constants";
-// import { CellFillType } from "./helper/constants";
+import MainHeader from "./components/MainHeader.vue";
+import { CellType, AlgoType } from "./helper/constants";
 
 let windowWidth = ref(window.innerWidth);
 let windowHeight = ref(window.innerHeight);
@@ -29,15 +46,22 @@ const BOARDROWS = 30;
 const BOARDCOLS = Math.round(windowWidth.value / 22);
 console.log(`No.of board rows are ${BOARDROWS}, and columns are ${BOARDCOLS}`);
 
+// const START = [15, 15];
+// const END = [15, 45];
+
 export default {
   name: "App",
   components: {
     CellItem,
+    MainHeader,
   },
   data() {
     return {
       board: [],
       cellKey: 0,
+      path: [],
+      isMouseDown: false,
+      algoType: AlgoType.DFS,
     };
   },
   mounted: function () {
@@ -61,6 +85,14 @@ export default {
         this.board.push(row);
       }
       this.cellKey = _cellKey;
+
+      this.board[12][5].cellType = CellType.Start;
+      this.board[12][33].cellType = CellType.End;
+
+      // var [r, c] = START;
+      // this.board[r][c].cellType = CellType.Start;
+      // [r, c] = END;
+      // this.board[r][c].cellType = CellType.End;
     },
     getAdjIndex(row, col) {
       return [
@@ -72,7 +104,7 @@ export default {
     },
     dfs(cellInfo) {
       setTimeout(() => {
-        cellInfo.cellType = CellType.Filled;
+        // cellInfo.cellType = CellType.Filled;
         const adjValues = this.getAdjIndex(cellInfo.row, cellInfo.col);
         for (let adjValue of adjValues) {
           let [x, y] = adjValue;
@@ -81,14 +113,44 @@ export default {
             //   `The clicked index is ${adjValue.row},${adjValue.col} and the adjacent values are ${adjValue}`
             // );
             const adjCellInfo = this.board[x][y];
-            if (adjCellInfo.cellType === CellType.Free) {
+
+            if (adjCellInfo.cellType === CellType.End) {
+              console.log(`Found the End Node!!`);
+              console.log(this.path);
+              return true;
+            } else if (adjCellInfo.cellType === CellType.Free) {
               adjCellInfo.cellType = CellType.Filled;
-              console.log(adjCellInfo.row, adjCellInfo.col);
-              this.dfs(adjCellInfo);
+              //console.log(adjCellInfo.row, adjCellInfo.col);
+              this.path.push([adjCellInfo.row, adjCellInfo.col]);
+              console.log(this.path);
+              if (this.dfs(adjCellInfo)) return true;
+              this.path.pop();
             }
           }
         }
       }, 25);
+    },
+    modifyCellType(cellInfo) {
+      if (
+        this.isMouseDown &&
+        cellInfo.cellType != CellType.Start &&
+        cellInfo.cellType != CellType.End
+      ) {
+        if (cellInfo.cellType == CellType.Wall) {
+          cellInfo.cellType = CellType.Free;
+        } else {
+          cellInfo.cellType = CellType.Wall;
+        }
+      } else {
+        console.log("Nothing Here!");
+      }
+    },
+    modifyCellTypeClick(cellInfo) {
+      if (cellInfo.cellType == CellType.Wall) {
+        cellInfo.cellType = CellType.Free;
+      } else {
+        cellInfo.cellType = CellType.Wall;
+      }
     },
   },
 };
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,47 @@
+<template>
+  <header>
+    <div class="title">Algo Vis</div>
+    <button class="btn">Start</button>
+  </header>
+</template>
+<script>
+import { CellType } from "@/helper/constants";
+
+export default {
+  name: "MainHeader",
+  data() {
+    return {
+      cellType: CellType.Filled,
+    };
+  },
+};
+</script>
+<style>
+header {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  padding: 10px;
+}
+
+.title {
+  font-size: 1.5rem;
+  /* padding-left: 10px;
+  padding-top: 5px; */
+}
+
+.btn {
+  font-size: 1.5rem;
+  background-color: blueviolet;
+  color: white;
+  /* padding: 5px 15px; */
+  border-radius: 10px;
+  border: 0;
+  cursor: pointer;
+  /* padding-right: 10px; */
+}
+
+.btn:hover {
+  border: 1px solid yellow;
+}
+</style>
