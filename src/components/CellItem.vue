6182274c014bc945121404b94b40c9c23e1c8e1d
Sainitin D <s.daverpally@gmail.com>
2024-02-18 10:53:15 -0500

    modify css styling and organize code for CellItem

--- src/components/CellItem.vue
+++ src/components/CellItem.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="cell" :style="getGridStyle" :class="selectCellStyling()"></div>
+  <div class="cell" :class="handleCellItemStyling()"></div>
+  <!-- :style="getGridStyle" -->
 </template>
 <script>
-import { CellType } from "@/helper/constants";
-
-// const rowStart = row + 1;
-// const rowEnd = row + 2;
+import { CellType } from "@/helper/Enums";
 
 export default {
   name: "CellItem",
@@ -24,23 +22,20 @@ export default {
     },
   },
   methods: {
-    isCellFree() {
-      return this.cellInfo.cellType === CellType.Free;
-    },
-    selectCellStyling() {
-      if (this.cellInfo.cellType === CellType.Free) {
-        return "";
-      } else if (this.cellInfo.cellType === CellType.Start) {
+    handleCellItemStyling() {
+      if (this.cellInfo.cellType === CellType.START) {
         return "cell-start";
-      } else if (this.cellInfo.cellType === CellType.End) {
+      } else if (this.cellInfo.cellType === CellType.END) {
         return "cell-end";
-      } else if (this.cellInfo.cellType === CellType.Wall) {
-        return "cell-wall";
-      } else if (this.cellInfo.cellType === CellType.Path) {
-        return "cell-path";
-      } else if (this.cellInfo.cellType === CellType.Filled) {
+      } else if (this.cellInfo.cellType === CellType.FREE) {
+        return "";
+      } else if (this.cellInfo.cellType === CellType.FILLED) {
         return "cell-filled";
-      } else if (this.cellInfo.cellType === CellType.FilledNoAnim) {
+      } else if (this.cellInfo.cellType === CellType.WALL) {
+        return "cell-wall";
+      } else if (this.cellInfo.cellType === CellType.PATH) {
+        return "cell-path";
+      } else if (this.cellInfo.cellType === CellType.FILLEDNOANIM) {
         return "cell-filled-noAnim";
       }
     },
@@ -52,13 +47,18 @@ export default {
   background-color: rgba(0, 190, 218, 0.75);
 }
 .cell {
-  width: 20px;
-  height: 20px;
-  border: 1px solid black;
+  width: 30px;
+  height: 30px;
+  /* margin: 5px; */
+  border: 1px solid grey;
 }
 
 .cell:hover {
-  background-color: grey;
+  /* box-shadow: 0 0 0.8em #26489a; */
+  /* border-color: #3c3e49; */
+  /* transform: scale(1.1); */
+  background-color: rgba(128, 128, 128, 0.32);
+  transition: 0.2s ease-in-out;
 }
 
 .cell-wall {
@@ -83,11 +83,11 @@ export default {
 }
 
 .cell-start {
-  background-color: green;
+  background-color: rgb(0, 255, 0);
 }
 
 .cell-end {
-  background-color: red;
+  background-color: rgb(255, 0, 0);
 }
 
 @keyframes visitedAnimation {
