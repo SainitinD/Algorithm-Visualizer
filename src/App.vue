8719298d82a2fe367cd47e2cc014a736df8399df
Sainitin D <s.daverpally@gmail.com>
2024-02-20 22:51:48 -0500

    revamp App.vue with modular code

--- src/App.vue
+++ src/App.vue
@@ -1,311 +1,94 @@
 <template>
   <div id="app">
-    <MainHeader @click="this.handleHeader()" />
-    <div id="board" v-if="this.board.length !== 0">
-      <div class="row" v-for="(row, rowIdx) in board" :key="rowIdx">
-        <CellItem
-          v-for="cellInfo in row"
-          :key="cellInfo.id"
-          :cellInfo="cellInfo"
-          @mousedown="this.handleMouseDown(cellInfo)"
-          @mouseenter="this.handleMouseEnter(cellInfo)"
-          @mouseup="this.handleMouseUp"
-        />
-      </div>
-    </div>
+    <MainHeader
+      @changeAlgoOptions="changeAlgoOptions"
+      @changeWallOptions="changeWallOptions"
+      @changeSpeedOptions="changeSpeedOptions"
+      @toggleAlgoRun="this.didAlgoRun = !this.didAlgoRun"
+      @toggleClearWalls="this.didClearWalls = !this.didClearWalls"
+      @toggleClearPath="this.didClearPath = !this.didClearPath"
+      :didAlgoRun="didAlgoRun"
+      @clearPath="didAlgoRun = false"
+    />
+    <BoardItem
+      :options="this.options"
+      :didAlgoRun="this.didAlgoRun"
+      :didClearWalls="didClearWalls"
+      :didClearPath="didClearPath"
+    />
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
-import CellItem from "./components/CellItem.vue";
 import MainHeader from "./components/MainHeader.vue";
-import { CellType, AlgoType } from "./helper/constants";
+import BoardItem from "./components/BoardItem.vue";
+import { AlgoType, SpeedType, WallType } from "./helper/Enums";
 
-let windowWidth = ref(window.innerWidth);
-let windowHeight = ref(window.innerHeight);
-console.log(
-  `Window width is ${windowWidth.value} and height is ${windowHeight.value}`
-);
-
-const BOARDROWS = 30;
-const BOARDCOLS = Math.round(windowWidth.value / 22);
-console.log(`No.of board rows are ${BOARDROWS}, and columns are ${BOARDCOLS}`);
+// path, visited, board,
 
 export default {
   name: "App",
   components: {
-    CellItem,
     MainHeader,
+    BoardItem,
   },
   data() {
     return {
-      board: [],
-      path: [],
-      isMouseDown: false,
-      cellTypeClicked: null,
-      lastVisitedCell: null,
-      algoType: AlgoType.DFS,
       didAlgoRun: false,
-      STARTINDEX: [12, 5],
-      ENDINDEX: [12, 33],
+      didClearWalls: false,
+      didClearPath: false,
+      options: {
+        algoType: AlgoType.DFS,
+        wallType: WallType.NOMAZE,
+        speed: SpeedType.FAST,
+      },
     };
   },
-  mounted: function () {
-    this.createBoard();
-  },
   methods: {
-    async handleHeader() {
-      if (this.didAlgoRun) {
-        await this.clearBoard();
-      } else {
-        await this.pathfind(true);
-      }
+    changeAlgoOptions: function (newVal) {
+      console.log(
+        `Changed algo type to ${this._getNameFromType("Algo", newVal)}`
+      );
+      this.options.algoType = newVal;
     },
-    createBoard() {
-      var _cellKey = 0;
-      for (let r = 0; r < BOARDROWS; r++) {
-        let row = [];
-        for (let c = 0; c < BOARDCOLS; c++) {
-          row.push({
-            id: _cellKey,
-            row: r,
-            col: c,
-            cellType: CellType.Free,
-          });
-          _cellKey += 1;
+    changeWallOptions: function (newVal) {
+      console.log(
+        `Changed wall type to ${this._getNameFromType("Wall", newVal)}`
+      );
+      this.options.wallType = newVal;
+    },
+    changeSpeedOptions: function (newVal) {
+      console.log(
+        `Changed speed type to ${this._getNameFromType("Speed", newVal)}`
+      );
+      this.options.speed = newVal;
+    },
+    _getNameFromType(type, val) {
+      if (type === "Algo") {
+        if (val == AlgoType.DFS) {
+          return "DFS";
+        } else if (val == AlgoType.BFS) {
+          return "BFS";
+        } else if (val == AlgoType.Djikstra) {
+          return "Dijkstra";
+        } else if (val == AlgoType.ASTAR) {
+          return "A*";
         }
-        this.board.push(row);
-      }
-      this.board[this.STARTINDEX[0]][this.STARTINDEX[1]].cellType =
-        CellType.Start;
-      this.board[this.ENDINDEX[0]][this.ENDINDEX[1]].cellType = CellType.End;
-
-      // var [r, c] = START;
-      // this.board[r][c].cellType = CellType.Start;
-      // [r, c] = END;
-      // this.board[r][c].cellType = CellType.End;
-    },
-    getAdjIndexes(row, col) {
-      return [
-        [row - 1, col],
-        [row, col + 1],
-        [row + 1, col],
-        [row, col - 1],
-      ];
-    },
-    async clearBoard() {
-      this.didAlgoRun = false;
-      // if (this.path.length == 0) return;
-      // for (let pathCell of this.path) {
-      //   let [r, c] = pathCell;
-      //   this.board[r][c].cellType = CellType.Free;
-      // }
-      // this.path = [];
-
-      this.path = [];
-      for (let r = 0; r < BOARDROWS; r++) {
-        for (let c = 0; c < BOARDCOLS; c++) {
-          if (
-            this.board[r][c].cellType != CellType.Start &&
-            this.board[r][c].cellType != CellType.End
-          ) {
-            this.board[r][c].cellType = CellType.Free;
-          }
+      } else if (type === "Wall") {
+        if (val == WallType.NOMAZE) {
+          return "No Maze";
+        } else if (val == WallType.RANDOMMAZE) {
+          return "Random Maze";
+        }
+      } else if (type === "Speed") {
+        if (val == SpeedType.SLOW) {
+          return "SLOW";
+        } else if (val == SpeedType.NORMAL) {
+          return "NORMAL";
+        } else if (val == SpeedType.FAST) {
+          return "FAST";
         }
       }
-      // await this.sleep(1);
-    },
-    async dfs(cellInfo, includeAnimation) {
-      // Error checking. Don't wanna run dfs if user didn't ask for it
-      if (!this.didAlgoRun) return false;
-
-      // Find the adjacent indices of the current cell
-      let adjIndexes = this.getAdjIndexes(cellInfo.row, cellInfo.col);
-
-      // Go through each cell and dfs
-      for (let [row, col] of adjIndexes) {
-        // logging for debugging
-        // console.log(
-        //   `The current cell is at ${row},${col} and its adjacent cells are ${adjIndexes}`
-        // );
-        if (!this.didAlgoRun) return false;
-
-        // edge case checks
-        if (!(0 <= row && row < BOARDROWS && 0 <= col && col < BOARDCOLS))
-          continue;
-
-        // more edge case checks
-        const adjCell = this.board[row][col];
-        if (
-          adjCell.cellType === CellType.Wall ||
-          adjCell.cellType === CellType.Start ||
-          adjCell.cellType === CellType.Filled ||
-          adjCell.cellType === CellType.FilledNoAnim
-        )
-          continue;
-
-        // return if we find the end node
-        if (adjCell.cellType === CellType.End) {
-          console.log("DFS: Found the end node!");
-          return true;
-        }
-
-        if (includeAnimation) {
-          // Otherwise, label the cell as filled and wait for css to change
-          adjCell.cellType = CellType.Filled;
-          await this.sleep(1);
-        } else {
-          adjCell.cellType = CellType.FilledNoAnim;
-        }
-
-        // perform the dfs
-        this.path.push([row, col]);
-        if (await this.dfs(adjCell, includeAnimation)) return true;
-        this.path.pop();
-      }
-      return false;
-    },
-    async drawPath(includeAnimation) {
-      if (this.path.length == 0) return;
-      for (let pathCell of this.path) {
-        let [r, c] = pathCell;
-        this.board[r][c].cellType = CellType.Path;
-        if (includeAnimation) await this.sleep(1);
-      }
-    },
-    handleMouseDown(cellInfo) {
-      this.isMouseDown = true;
-      // record details on the cell clicked
-      this.cellTypeClicked = cellInfo.cellType;
-      this.lastVisitedCell = cellInfo;
-      console.log("Mouse Down");
-    },
-    async handleMouseEnter(cellInfo) {
-      if (!this.isMouseDown) return;
-
-      // functionality to draw the walls and move start and end nodes
-      if (
-        this.cellTypeClicked == CellType.Start &&
-        cellInfo.cellType != CellType.End
-      ) {
-        cellInfo.cellType = CellType.Start;
-        this.lastVisitedCell = cellInfo;
-        this.STARTINDEX = [cellInfo.row, cellInfo.col];
-        if (this.didAlgoRun) {
-          await this.clearBoard();
-          this.dfsWithoutAnim();
-        }
-        console.log(`Start Cell changed to ${cellInfo.row}, ${cellInfo.col}`);
-      } else if (
-        this.cellTypeClicked == CellType.End &&
-        cellInfo.cellType != CellType.Start
-      ) {
-        cellInfo.cellType = CellType.End;
-        this.lastVisitedCell = cellInfo;
-        this.ENDINDEX = [cellInfo.row, cellInfo.col];
-        if (this.didAlgoRun) {
-          await this.clearBoard();
-          this.dfsWithoutAnim();
-        }
-        console.log(`End Cell changed to ${cellInfo.row}, ${cellInfo.col}`);
-      } else if (
-        cellInfo.cellType != CellType.Start &&
-        cellInfo.cellType != CellType.End
-      ) {
-        if (cellInfo.cellType == CellType.Wall) {
-          cellInfo.cellType = CellType.Free;
-        } else {
-          cellInfo.cellType = CellType.Wall;
-        }
-      } else {
-        console.log("Nothing Here!");
-      }
-    },
-    handleMouseUp() {
-      this.isMouseDown = false;
-      console.log("Mouse Up");
-    },
-    dfsWithoutAnim() {
-      const visited = [];
-      this.didAlgoRun = true;
-      const dfs = (cellInfo) => {
-        // Error checking. Don't wanna run dfs if user didn't ask for it
-        if (!this.didAlgoRun) return false;
-        // Find the adjacent indices of the current cell
-        let adjIndexes = this.getAdjIndexes(cellInfo.row, cellInfo.col);
-        // Go through each cell and dfs
-        for (let [row, col] of adjIndexes) {
-          // edge case checks
-          if (!this.didAlgoRun) return false;
-          if (!(0 <= row && row < BOARDROWS && 0 <= col && col < BOARDCOLS)) {
-            continue;
-          }
-
-          const adjCell = this.board[row][col];
-          if (
-            adjCell.cellType === CellType.Wall ||
-            adjCell.cellType === CellType.Start
-          )
-            continue;
-
-          if (
-            visited.findIndex(
-              (x) => x[0] == adjCell.row && x[1] == adjCell.col
-            ) !== -1
-          )
-            continue;
-
-          // return if we find the end node
-          if (adjCell.cellType === CellType.End) {
-            console.log("DFS: Found the end node!");
-            return true;
-          }
-          visited.push([adjCell.row, adjCell.col]);
-
-          // perform the dfs
-          this.path.push([row, col]);
-          if (dfs(adjCell)) return true;
-          this.path.pop();
-        }
-        return false;
-      };
-      dfs(this.board[this.STARTINDEX[0]][this.STARTINDEX[1]]);
-      this.drawPath();
-    },
-
-    async pathfind(includeAnimation) {
-      // clear the board if previous pathfinding is displayed
-      console.log("Starting the path finding!");
-      // if (this.didAlgoRun) {
-      //   this.didAlgoRun = false;
-      //   this.clearBoard();
-      //   return;
-      // }
-
-      // perform the path finding
-      this.didAlgoRun = true;
-      if (
-        await this.dfs(
-          this.board[this.STARTINDEX[0]][this.STARTINDEX[1]],
-          includeAnimation
-        )
-      ) {
-        console.log("Successfully returned");
-        await this.drawPath(includeAnimation);
-      } else {
-        console.log("Path cannot be found!!");
-      }
-    },
-    sleep(milliseconds) {
-      return new Promise((resolve) => setTimeout(resolve, milliseconds));
-    },
-  },
-  watch: {
-    lastVisitedCell: function (newVal, oldVal) {
-      if (oldVal == null) return;
-      if (newVal.cellType != oldVal.cellType) return;
-      oldVal.cellType = CellType.Free;
     },
   },
 };
@@ -315,15 +98,16 @@ export default {
 * {
   margin: 0px;
   padding: 0px;
+  font-family: Helvetica, sans-serif;
+}
+
+body {
+  background-color: #23242a;
 }
 
 #board {
-  display: grid;
+  /* display: grid; */
   gap: 0px;
   cursor: default;
-}
-
-.row {
-  display: grid;
 }
 </style>
